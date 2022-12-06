a2f77920bf29c3fe9ed7ddbaddda34e480cb6466
Sefa Umut Sarıkaya <sefaumutsarikaya@gmail.com>
2022-12-06 19:45:44 +0300

    whislist'ten checkout yapma

--- js/wishlist.js
+++ js/wishlist.js
@@ -2,6 +2,35 @@ let cart = localStorage.getItem("cartHeart")
   ? JSON.parse(localStorage.getItem("cartHeart"))
   : [];
 
+let cartSelect = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : [];
+const products = JSON.parse(localStorage.getItem("products"));
+
+
+const checkedOut = () => {
+  const btns = document.querySelectorAll(".addedChart");
+  let cartItems = document.querySelector(".header-cart-count");
+  btns.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+      e.preventDefault();
+      const id = e.target.dataset.id;
+      const product = products.find((product) => product.id === Number(id));
+      const inCart = cartSelect.find((item) => item.id === Number(id));
+      if (inCart) {
+        cartSelect = cartSelect.map((item) => {
+          return item.id === Number(id)
+
+            ? { ...item, quantity: item.quantity + 1 }
+            : item;
+        });
+      } else {
+        cartSelect = [...cartSelect, { ...product, quantity: 1 }];
+      }
+      localStorage.setItem("cart", JSON.stringify(cartSelect));
+      cartItems.innerHTML = cartSelect.length;
+    });
+  });
+};
+
 function displayCartProduct() {
   const cartWrapper = document.querySelector(".cart-wrapper");
   let result = "";
@@ -19,13 +48,14 @@ function displayCartProduct() {
         item.price.newPrice * item.quantity
       ).toFixed(2)}</td>
     <td> 
-      <button class="btn btn-primary btn-sm" data-id=${item.id}>Add to cart</button>
+      <button class="btn btn-primary btn-sm addedChart" data-id=${item.id}>Add to cart</button>
     </td>
    </tr>
    
    `;
   });
   cartWrapper.innerHTML = result;
+  checkedOut();
 }
 
 displayCartProduct();
